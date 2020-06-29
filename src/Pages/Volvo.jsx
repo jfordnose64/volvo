8a89a4121e96776c36c38f000ecae415fa570485
jackson ford <jfordnose64@gmail.com>
2020-06-29 15:51:34 -0400

    added a new items part to the home page

--- src/Pages/Volvo.jsx
+++ src/Pages/Volvo.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import CardSale from '../components/Card'
+import Image from '../Pictures/image.jpg'
+import data from '../data/test.json'
 
 class Volvo extends Component {
   render() {
+    const newModel = data.map((data) => {
+      return (
+        <div>
+          <CardSale
+            image={data.image}
+            title={data.title}
+            details="This is my POS"
+            buttonName="Name1"
+          />
+        </div>
+      )
+    })
+
     return (
       <div className="app">
         <Header />
         <h1 className="header">Volvo Docs </h1>
+        <div className="img-container">{newModel}</div>
       </div>
     )
   }
